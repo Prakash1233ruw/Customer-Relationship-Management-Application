26a7826c9f37413565f28decf170b6d3d9ce12cd
prakash <pr0018908@gmail.com>
2024-02-08 17:29:14 +0530

    main

---
+++ src/main/java/com/pp/SpringMVCCRMApplication.java
@@ -0,0 +1,15 @@
+package com.pp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class SpringMVCCRMApplication {
+
+	public static void main(String[] args) {
+	 SpringApplication.run(SpringMVCCRMApplication.class, args);
+	
+	}
+
+}
